479abf23d635bf772faa1ab632302522824be386
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-12-02 21:55:46 +0300

    loading the background and using gdscript to pick a random background and scroll

---
+++ Scene/UserInterface/background.gd
@@ -0,0 +1,11 @@
+extends ParallaxBackground
+
+@onready var sky: Sprite2D = get_node("Sky/Background")
+const scroll_speed = 80
+func _ready() -> void:
+	randomize()
+	var index = randi_range(1, 2)
+	sky.texture = load("res://Assets/Sprite/background-" + str(index) + ".png")
+
+func _process(delta: float) -> void:
+	scroll_offset.x -= scroll_speed * delta
